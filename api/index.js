fcd8dc2b51c8676ac10ef545ed9eb672c082124e
Sunny <islamarafat171@gmail.com>
2022-06-23 21:27:30 +0200

    deployed client in vercel

--- api/index.js
+++ api/index.js
@@ -33,7 +33,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors({
     credentials: true,
-    origin: ["http://localhost:3000"]
+    origin: ["https://client-five-blue.vercel.app"]
 }))
 
 app.set("trust proxy", 1);
