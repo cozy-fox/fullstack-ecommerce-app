551c51ca5c90bc89cb24043ff70a21a4fd871bbe
Sunny <islamarafat171@gmail.com>
2022-06-23 10:40:09 +0200

    prepearing for deployment

--- api/index.js
+++ api/index.js
@@ -46,6 +46,10 @@ app.get('/api/publishableKey', (req, res) => {
     res.status(200).json({ key: process.env.STRIPE_PUBIC })
 })
 
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Hello from API' })
+})
+
 app.use(errorHandler)
 
 app.listen(port, () => {
