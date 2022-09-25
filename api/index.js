0cc37f857e43f80f2d3252a93f07eda7257a0dd5
Sunny <islamarafat171@gmail.com>
2022-09-25 23:52:02 +0200

    Fixed bug in dirname.

--- api/index.js
+++ api/index.js
@@ -14,6 +14,10 @@ import cartRouter from "./routes/cartRoutes.js"
 import checkoutRouter from "./routes/checkoutRoutes.js"
 import orderRouter from "./routes/orderRoutes.js"
 import path from "path"
+import { fileURLToPath } from "url"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 dotenv.config()
 const app = express()
